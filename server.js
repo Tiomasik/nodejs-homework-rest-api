5d95f218dabcbfedecf5ac51f4e59e6600201e07
Artem Maslii <a.masliy@ukr.net>
2023-04-14 21:19:45 +0300

    start

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
-    console.log("Database connection successful");
+    console.log("Databases connection successful");
   })
   .catch((error) => {
     console.log(error.message);
